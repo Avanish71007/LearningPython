55923e38583db4b6e1fcf4ace53d0700fb48a8c1
Avanish71007 <avanishchaurasiya71@gmail.com>
2024-12-28 19:35:38 +0530

    update srting_upper.py

---
+++ strings/srting_upper.py
@@ -0,0 +1,8 @@
+name="Avanish Chaurasiya"
+print("Here is the string: \t"+ name)
+
+print('Here is the string in lower case: \n')
+print(name.lower())  #to print in small letters (only print in small but doest change the object bevause string is immutable )
+
+print("Here is the same string in capital case: \n")
+print(name.upper)  #to print in upper case
